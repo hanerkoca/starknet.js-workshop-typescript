36a11318584858532b74bc97394fb86cb259ad8b
Philippe ROSTAN <81040730+PhilippeR26@users.noreply.github.com>
2023-03-01 18:54:59 +0100

    build: signature and hash

---
+++ src/scripts/signature/1.signatureTestV4.ts
@@ -0,0 +1,16 @@
+// V4
+import { ec, encode, hash } from 'starknet';
+
+const privateKey = '0x5b7d4f8710b3581ebb2b8b74efaa23d25ab0ffea2a4f3e269bf91bf9f63d634';
+const fullPubKey = encode.addHexPrefix(keyPair.getPublic("hex")); // 512 bits
+const starknetPubKey: string = ec.getStarkKey(keyPair); // 256 bits
+console.log('fullpubKey    =', fullPubKey);
+console.log('starknetPubKey= ', starknetPubKey);
+
+const message = ['0x53463473467', '0x879678967', '0x67896789678'];
+const msgHash = hash.computeHashOnElements(message);
+const signature = ec.sign(keyPair, msgHash);
+
+const inferredKeyPair = ec.getKeyPairFromPublicKey(fullPubKey);
+const isVerified = ec.verify(inferredKeyPair, msgHash, signature);
+console.log('isVerified =', isVerified);
