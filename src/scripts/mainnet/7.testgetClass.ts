cddc90988534fb2b4d81bdca25a9e9158f7398a1
Philippe ROSTAN <81040730+PhilippeR26@users.noreply.github.com>
2023-06-23 17:16:22 +0200

    build: transfer class

--- src/scripts/mainnet/7.testgetClass.ts
+++ src/scripts/mainnet/7.testgetClass.ts
@@ -1,9 +1,8 @@
 // Connect to a Pathfinder Mainnet node
 // Launch with npx ts-node src/scripts/mainnet/7.testgetClass.ts
-// Coded with Starknet.js v5.13.0
+// Coded with Starknet.js v5.14.0 #00b7bdf756c516a1905ce1ecc9036472800989af
 
-import { Provider, RpcProvider, Contract, Account, json, uint256, Abi, constants, shortString, CompiledContract, ContractClass, RPC, SequencerProvider, } from "starknet";
-import { accountMainnet4Address, accountMainnet4AddressprivateKey } from "../../A-MainPriv/mainPriv";
+import { Provider, RpcProvider, Contract, Account, json, uint256, Abi, constants, shortString, CompiledContract, ContractClass, RPC, SequencerProvider, ContractClassResponse, stark, contractClassResponseToLegacyCompiledContract } from "starknet";
 import { alchemyKey } from "../../A-MainPriv/mainPriv";
 import { resetDevnetNow } from "../resetDevnetFunc";
 import fs from "fs";
@@ -19,15 +18,12 @@ async function main() {
     const providerMainnetRpc = new Provider({ rpc: { nodeUrl: 'http://192.168.1.99:9545' } });
     // on the same computer : const provider = new Provider({ rpc: { nodeUrl: 'http://127.0.0.1:9545' } });
     const providerMainnetSequencer = new SequencerProvider({ network: constants.NetworkName.SN_MAIN });
-    // const provider = new Provider({ sequencer: { network: constants.NetworkName.SN_GOERLI } });
+    // const providerTestnet = new Provider({ sequencer: { network: constants.NetworkName.SN_GOERLI } });
     const providerDevnet = new Provider({ sequencer: { baseUrl: "http://127.0.0.1:5050" } });
 
     const chainId = await providerMainnetRpc.getChainId();
     console.log('Connected to the local network node (Mainnet)=', chainId);
 
-    // connect existing ArgentX account 4 in Mainnet
-    const account4 = new Account(providerMainnetRpc, accountMainnet4Address, accountMainnet4AddressprivateKey);
-    console.log('existing ArgentX account4 connected. Address =', account4.address, "\n");
 
     // connect account 0 in devnet
     const privateKey0 = "0xe3e70682c2094cac629f6fbed82c07cd";
@@ -38,43 +34,50 @@ async function main() {
 
     // re-initialization of devnet
     await resetDevnetNow();
-    // **************** Cairo 0 **************
-    const addrErc20Cairo0 = "0x03d71989510013c4c4853a176325588bb84eaeb8886f032dd15d7ad2935a9e33";
-    const classHashErc20Cairo0 = "0x00bbcdb339b2d436ddc8174c661cc6de29786f93e9d3b56a16bd658f306eb12a";
-    const compiledContract0 = await providerMainnetRpc.getClassAt(addrErc20Cairo0);
-    const compiledContract00 = await providerMainnetRpc.getClassByHash(classHashErc20Cairo0);
-    fs.writeFileSync("./c0contract.json", json.stringify(compiledContract0, undefined, 2));
-    const compiledC0 = json.parse(fs.readFileSync("./c0contract.json").toString("ascii"));
-    const abi0 = compiledContract0.abi;
-    // console.log("abi =", compiledC0.abi, "===")
-   const deployResponse0 = await account0.declareAndDeploy({ contract: compiledC0 });
-    const deployResponse00 = await account0.declare({ contract: compiledContract00  });
-
-    const contractClassHash0 = deployResponse0.declare.class_hash;
-    console.log('✅ Contract0 declared with classHash =', contractClassHash0);
-    console.log("contract0_address =", deployResponse0.deploy.contract_address);
-    await providerDevnet.waitForTransaction(deployResponse0.deploy.transaction_hash);
-
-    // process.exit(4);
 
     // ***************Cairo 1 
     const addrErc20Cairo1 = "0x060cf64cf9edfc1b16ec903cee31a2c21680ee02fc778225dacee578c303806a";
     const classHashErc20Cairo1 = "0x021b9a01b8f17682f92c18ac4fa08ea90b371f07e27cd08de8451892546d341f";
 
-    const compiledSierra1 = await providerMainnetRpc.getClassAt(addrErc20Cairo0); // with sequencer or rpc
-    fs.writeFileSync("./c1_Sierra.json", json.stringify(compiledContract0, undefined, 2));
+    const compiledSierra1: ContractClassResponse = await providerMainnetRpc.getClassAt(addrErc20Cairo1); // with sequencer or rpc
+    fs.writeFileSync("./c1_Sierra.json", json.stringify(compiledSierra1, undefined, 2));
     const compiledCasm1 = await providerMainnetSequencer.getCompiledClassByClassHash(classHashErc20Cairo1); // only with a sequencer
     fs.writeFileSync("./c1_Casm.json", json.stringify(compiledCasm1, undefined, 2));
 
-    const compiledC1 = json.parse(fs.readFileSync("./c1Sierra.json").toString("ascii"));
-    // const abi0:Abi=compiledContract0.abi as Abi;
-    console.log("abi =", compiledSierra1.abi, "===")
-    const deployResponse1 = await account0.declareAndDeploy({ contract: compiledSierra1,casm: compiledCasm1 });
+    const compiledC1 = json.parse(fs.readFileSync("./c1_Sierra.json").toString("ascii"));
+    const abi1: Abi = compiledSierra1.abi as Abi;
+    //console.log("abi =", compiledSierra1.abi, "===")
+    const compiledC12 = compiledSierra1 as CompiledContract;
+    const deployResponse1 = await account0.declare({ contract: compiledC12, casm: compiledCasm1 });
 
-    const contractClassHash1 = deployResponse1.declare.class_hash;
+    const contractClassHash1 = deployResponse1.class_hash;
     console.log('✅ Contract1 declared with classHash =', contractClassHash1);
-    console.log("contract1_address =", deployResponse1.deploy.contract_address);
-    await providerDevnet.waitForTransaction(deployResponse1.deploy.transaction_hash);  
+    //console.log("contract1_address =", deployResponse1.deploy.contract_address);
+    await providerDevnet.waitForTransaction(deployResponse1.transaction_hash);
+
+    // **************** Cairo 0 **************
+    const addrErc20Cairo0 = "0x03d71989510013c4c4853a176325588bb84eaeb8886f032dd15d7ad2935a9e33";
+    const classHashErc20Cairo0 = "0x00bbcdb339b2d436ddc8174c661cc6de29786f93e9d3b56a16bd658f306eb12a";
+    const compressedContract0 = await providerMainnetRpc.getClassAt(addrErc20Cairo0);
+    const compiledContract0 = contractClassResponseToLegacyCompiledContract(compressedContract0);
+    //const compressedContract00: ContractClassResponse = await providerMainnetRpc.getClassByHash(classHashErc20Cairo0);
+    //if (!("program" in compressedContract00)) { throw Error("Not C0 contract") };
+    //const compiledContract00: CompiledContract = { ...compressedContract00, program: stark.decompressProgram(compressedContract00.program) }
+    fs.writeFileSync("./c0contract.json", json.stringify(compiledContract0, undefined, 2));
+    const compiledC0 = json.parse(fs.readFileSync("./c0contract.json").toString("ascii"));
+    const abi0 = compiledContract0.abi;
+    // console.log("abi =", compiledC0.abi, "===")
+    const deployResponse0 = await account0.declare({ contract: compiledContract0 });
+    // const deployResponse00 = await account0.declare({ contract: compiledC0 });
+
+    const contractClassHash0 = deployResponse0.class_hash;
+    console.log('✅ Contract0 declared with classHash =', contractClassHash0);
+    // console.log("contract0_address =", deployResponse0.contract_address);
+    await providerDevnet.waitForTransaction(deployResponse0.transaction_hash);
+
+    // process.exit(4);
+
+
     console.log("✅ Test completed.");
 
 }
