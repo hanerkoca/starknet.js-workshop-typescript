0f0795d6949dc281a8e105f8aee427f37b891899
Philippe ROSTAN <81040730+PhilippeR26@users.noreply.github.com>
2023-09-19 15:46:53 +0200

    feature: private key and address from passphrase

--- src/scripts/argentX/4.getPrivFromArgentXSeed.ts
+++ src/scripts/argentX/4.getPrivFromArgentXSeed.ts
@@ -1,11 +1,13 @@
 // Calculate private key from ArgentX seed phrase.
 // Coded with Starknet.js v5.19.5, starknet-devnet 0.6.1
 // launch with npx ts-node src/scripts/argentX/4.getPrivFromArgentXSeed.ts
+import { CallData, ec, hash } from "starknet";
 import * as mStarknet from '@scure/starknet';
 import * as bip32 from "@scure/bip32";
 import * as bip39 from '@scure/bip39';
 
 const mnemonic = "inquiry tuition toe harvest vanish dress doctor maid divorce mystery cross loyal";
+const contractAXclassHash = "0x1a736d6ed154502257f02b1ccdf4d9d1089f80811cd6acad48e6b6a9d1f2003";
 
 const masterSeed = bip39.mnemonicToSeedSync(mnemonic);
 const hdKey1 = bip32.HDKey.fromMasterSeed(masterSeed).derive("m/44'/60'/0'/0/0");
@@ -18,4 +20,8 @@ for (let i = 0; i < 5; i++) {
     console.log("path =", path);
     const starknetPrivateKey = "0x" + mStarknet.grindKey(hdKeyi.privateKey!);
     console.log("privateKey =", starknetPrivateKey);
+    const starkKeyPubAX = ec.starkCurve.getStarkKey(starknetPrivateKey);
+    const constructorAXCallData=CallData.compile([starkKeyPubAX,0]);
+    const accountAXAddress = hash.calculateContractAddressFromHash(starkKeyPubAX, contractAXclassHash, constructorAXCallData, 0);
+    console.log('Account address=', accountAXAddress);
 }
