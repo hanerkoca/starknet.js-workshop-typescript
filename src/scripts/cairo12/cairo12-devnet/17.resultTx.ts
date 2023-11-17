1fe78f23a394fb139671dc3dbbd8e10378e303e5
PhilippeR26 <philippe.rostan@yahoo.fr>
2023-11-17 11:37:26 +0100

    feat: add deploy ArgentX account in testnet

--- src/scripts/cairo12/cairo12-devnet/17.resultTx.ts
+++ src/scripts/cairo12/cairo12-devnet/17.resultTx.ts
@@ -7,79 +7,30 @@ import { infuraKey } from "../../../A-MainPriv/mainPriv";
 import { account7TestnetPrivateKey, junoNMtestnet, account5TestnetAddress, account5TestnetPrivateKey } from "../../../A1priv/A1priv";
 import { addrETH } from "../../../A2priv/A2priv";
 
-import fs from "fs";
 import axios from "axios";
 import * as dotenv from "dotenv";
 import { resetDevnetNow } from "../../resetDevnetFunc";
 dotenv.config();
 
 
-// export interface Success {
-//     tag: "success"
-//     value: SuccessfulTransactionReceiptResponse
-// }
-// export interface Rejected {
-//     tag: "rejected"
-//     value: RejectedTransactionReceiptResponse
-// }
-// export interface Reverted {
-//     tag: "reverted"
-//     value: RevertedTransactionReceiptResponse
-// }
-// export type ResultTx = Success | Rejected | Reverted;
-
-// export namespace ResultTx {
-//     class Success implements Success {
-//         public tag: "success" = "success";
-//         public value: SuccessfulTransactionReceiptResponse;
-//         public constructor(val: SuccessfulTransactionReceiptResponse) {
-//             this.value = val
-//         }
-//     }
-//     class Rejected implements Rejected {
-//         public tag: "rejected" = "rejected";
-//         public value: RejectedTransactionReceiptResponse;
-//         public constructor(val: RejectedTransactionReceiptResponse) {
-//             this.value = val
-//         }
-//     }
-//     class Reverted implements Reverted {
-//         public tag: "reverted" = "reverted";
-//         public value: RevertedTransactionReceiptResponse;
-//         public constructor(val: RevertedTransactionReceiptResponse) {
-//             this.value = val
-//         }
-//     }
-// }
-
 interface Matchable<T> {
     match<E>(fns: { some: (t: T) => E, none: () => E }): E;
 }
 
-export interface Some<T>  {
+export interface Somee<T> extends Matchable<T>{
     tag: "some"
     value: T
 }
 
-export interface None  {
+export interface Nonee extends Matchable<never>{
     tag: "none"
 }
 
-export type Option<T> = (Some<T> | None) //& Matchable<T>
+export type Optione<T> = (Somee<T> | Nonee) & Matchable<T>
 
 export namespace Option {
-    // interface Some<T> {
-    //     tag: "some"
-    //     value: T
-    // }
     
-    // interface None {
-    //     tag: "none"
-    // }
-    
-    // type Option<T> = (Some<T> | None) 
-
-    class Some<T> implements Some<T> {
+    class Some<T> implements Somee<T> {
         public tag: "some" = "some"
         public value: T
 
@@ -90,9 +41,13 @@ export namespace Option {
         public toString() {
             return `Some{ ${this.value} }`
         }
+
+        match<E>(fns: { some: (t: T) => E; none: () => E; }): E {
+            return 
+        }
     }
 
-    class None implements None {
+    class None implements Nonee {
         public tag: "none" = "none"
 
         public toString() {
@@ -101,29 +56,29 @@ export namespace Option {
     }
 
 
-export function none(): None {
-    return new None()
-}
-
-export function of<T>(val: T): Option<T> {
-    if (val === null || val === undefined) {
-        return Option.none()
+    export function none(): None {
+        return new None()
     }
 
-    return new Some(val)
-}
+    export function of<T>(val: T): Optione<T> {
+        if (val === null || val === undefined) {
+            return Option.none()
+        }
+
+        return new Some(val)
+    }
 
 }
 
-// const val:number=23;
-const val:string="abc";
-const maybe=Option.of(val);
-const maybe2=Option.of(null);
-console.log("maybe=",maybe,maybe2);
-// maybe.match({
-//     some(num:number){console.log("some=",num)},
-//     none(){console.log("problem")}
-// })
+const val:number=23;
+//const val: string = "abc";
+const maybe = Option.of(val);
+const maybe2 = Option.of(null);
+console.log("maybe=", maybe, maybe2);
+maybe.match({
+    some(num:number){console.log("some=",num)},
+    none(){console.log("problem")}
+})
 
 
 async function main() {
@@ -144,7 +99,7 @@ async function main() {
 
 
 
-    const v=account0.getCairoVersion()
+    const v = account0.getCairoVersion()
 
     console.log('✅ contract deployed.');
 
---
+++ src/scripts/cairo12/cairo12-testnet/4.DeployNewArgentXaccount.ts
@@ -0,0 +1,73 @@
+// Deploy in Testnet a new ArgentX wallet (Cairo1 0.3.0).
+// launch with : npx ts-node src/scripts/cairo12/cairo12-testnet/4.DeployNewArgentXaccount.ts
+// Coded with Starknet.js v5.21.0
+
+import { RpcProvider, Account, ec, json, stark, hash, CallData, Contract, cairo, Call } from "starknet";
+import { account2TestnetAddress, account2TestnetPrivateKey } from "../../../A1priv/A1priv";
+import fs from "fs";
+import { ethAddress } from "../../utils/constants";
+
+async function main() {
+    const provider = new RpcProvider({ nodeUrl: "https://starknet-testnet.public.blastapi.io" });
+    console.log("Provider connected.");
+    const chId = await provider.getChainId(); console.log("chId =", chId);
+
+    // initialize existing predeployed account 0 of Devnet
+
+    const privateKey0: string = account2TestnetPrivateKey;
+    const accountAddress0: string = account2TestnetAddress;
+    const account0 = new Account(provider, accountAddress0, privateKey0);
+    console.log("Account 0 connected.\n");
+
+    const accountAXsierra = json.parse(fs.readFileSync("./compiledContracts/cairo200/ArgentXaccount030.sierra.json").toString("ascii"));
+    const ch = hash.computeContractClassHash(accountAXsierra);
+    console.log("Class Hash of ArgentX contract =", ch);
+
+    // Calculate future address of the ArgentX account
+    const privateKeyAX = stark.randomAddress();;
+    console.log('AX account Private Key =', privateKeyAX);
+    console.log("Store safely this private key!!!");
+    const starkKeyPubAX = ec.starkCurve.getStarkKey(privateKeyAX);
+    console.log('AX account Public Key  =', starkKeyPubAX);
+
+
+    const contractAXclassHash = "0x1a736d6ed154502257f02b1ccdf4d9d1089f80811cd6acad48e6b6a9d1f2003"; // ArgentX Cairo 1 v0.3.0
+
+    const calldataAX = new CallData(accountAXsierra.abi);
+    const ConstructorAXCallData = calldataAX.compile("constructor", {
+        owner: starkKeyPubAX,
+        guardian: "0"
+    });
+    const accountAXAddress = hash.calculateContractAddressFromHash(starkKeyPubAX, contractAXclassHash, ConstructorAXCallData, 0);
+    console.log('Precalculated account address=', accountAXAddress);
+
+    // fund account address before account creation
+    const transferCall: Call = {
+        contractAddress: ethAddress,
+        entrypoint: "transfer",
+        calldata: CallData.compile([accountAXAddress, cairo.uint256(5 * 10 ** 15)])
+    };
+    const txHtransfer = await account0.execute(transferCall);
+    console.log("new account funded."); // 0.005 ETH
+    await provider.waitForTransaction(txHtransfer.transaction_hash);
+
+    // deploy ArgentX account
+    const accountAX = new Account(provider, accountAXAddress, privateKeyAX);
+    const deployAccountPayload = {
+        classHash: contractAXclassHash,
+        constructorCalldata: ConstructorAXCallData,
+        contractAddress: accountAXAddress,
+        addressSalt: starkKeyPubAX
+    };
+    const { transaction_hash: AXdAth, contract_address: accountAXFinalAdress } = await accountAX.deployAccount(deployAccountPayload);
+    console.log("Final address =", accountAXFinalAdress);
+    await provider.waitForTransaction(AXdAth);
+    console.log('✅ ArgentX wallet deployed.');
+
+}
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
