dc344adac3b99ae8501561089afb28c1ad8e9498
haner <77988014+hanerkoca@users.noreply.github.com>
2024-01-09 13:46:08 +0100

    Update formatBalance.ts

--- src/scripts/utils/formatBalance.ts
+++ src/scripts/utils/formatBalance.ts
@@ -1,6 +1,7 @@
-export function formatBalance(qty: bigint, decimals: number): string {
-    const balance = String("0").repeat(decimals) + qty.toString();
-    const rightCleaned = balance.slice(-decimals).replace(/(\d)0+$/gm, '$1');
-    const leftCleaned = BigInt(balance.slice(0, balance.length - decimals)).toString();
+export function formatBalance(qty: bigint, decimalsObj: { decimals: bigint }): string {
+    const decimalPlaces = Number(decimalsObj.decimals);
+    const balance = String("0").repeat(decimalPlaces) + qty.toString();
+    const rightCleaned = balance.slice(-decimalPlaces).replace(/(\d)0+$/gm, '$1');
+    const leftCleaned = BigInt(balance.slice(0, balance.length - decimalPlaces)).toString();
     return leftCleaned + "." + rightCleaned;
 }
